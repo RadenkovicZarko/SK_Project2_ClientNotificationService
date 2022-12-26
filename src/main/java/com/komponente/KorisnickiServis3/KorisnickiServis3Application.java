b9eff4e5f848afddcd6a0f29980aab0478139553
Zarko Radenkovic <zradenkovic520rn@raf.rs>
2022-12-26 01:18:29 +0100

    Initial commit

---
+++ src/main/java/com/komponente/KorisnickiServis3/KorisnickiServis3Application.java
@@ -0,0 +1,13 @@
+package com.komponente.KorisnickiServis3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KorisnickiServis3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KorisnickiServis3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/komponente/KorisnickiServis3/KorisnickiServis3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.komponente.KorisnickiServis3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KorisnickiServis3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
